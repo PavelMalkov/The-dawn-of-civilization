9b3c2050436548e1f661169136f8a7609f21e3d9
PavelMalkov <51037335+PavelMalkov@users.noreply.github.com>
2020-07-15 20:31:38 +1000

    Создан Скрол бокс, работет.
    Нужно теперь созать класс здания, задать информацию хранящуюся, и также определить все UI объекты.

---
+++ Assets/ScrowStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrowStart : MonoBehaviour
+{
+    public Scrollbar scrollbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollbar.value = 1;
+    }
+}
